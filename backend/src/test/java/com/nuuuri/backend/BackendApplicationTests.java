659243cf3d8f3d0ba5e0d9e9b51e7275989ae943
nuuuri <sbfl125@gmail.com>
2022-09-22 13:11:38 +0900

    Create Spring Boot Project

---
+++ backend/src/test/java/com/nuuuri/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nuuuri.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
